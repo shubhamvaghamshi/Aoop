40ab7af0be68c127e65a0d8a4ba45990ca76b89d
shubhamvaghamshi <162270392+shubhamvaghamshi@users.noreply.github.com>
2025-04-04 12:03:40 +0530

    Create Practical7.java

---
+++ Practical7.java
@@ -0,0 +1,15 @@
+
+
+   public class practical7{
+     double height;
+     double weight;
+     public practical7(double height , double weight){
+         this.height = height;
+         this.weight = weight;
+     }
+     public static void main(String[] args) {
+         practical7 x1 = new practical7(43.67 , 76.99);
+         System.out.println(x1.height);
+         System.out.println(x1.weight);
+     }
+ }
