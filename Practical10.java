1f222048351cc107cd8d7a90f09c70b8a449d40b
shubhamvaghamshi <162270392+shubhamvaghamshi@users.noreply.github.com>
2025-04-04 12:15:00 +0530

    Create Practical10.java

---
+++ Practical10.java
@@ -0,0 +1,16 @@
+class Shubham {
+ 
+    final int MAX_VALUE = 100;
+
+    final void displayMessage() {
+        System.out.println("This is a final method.");
+    }
+
+    public static void main(String[] args) {
+        Shubham s1 = new Shubham();
+ 
+        System.out.println("MAX_VALUE: " + s1.MAX_VALUE);
+
+        s1.displayMessage();
+    }
+}
