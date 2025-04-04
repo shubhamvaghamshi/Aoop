ad7bb2c68a5d44764a10281a05d99bdfb1a00579
shubhamvaghamshi <162270392+shubhamvaghamshi@users.noreply.github.com>
2025-04-04 12:17:05 +0530

    Create Practical 11.java

---
+++ Practical 11.java
@@ -0,0 +1,26 @@
+class Shape {
+
+    static final double PI = 3.14;
+
+    void Area(float r) {
+     double A = PI * r * r;
+     System.out.println("Area of the circle is: " + A);
+ }
+
+   void Area(float l, float w) {
+     double A = l * w;
+     System.out.println("Area of the rectangle is: " + A);
+ }
+}
+
+class AAA {
+
+ public static void main(String[] args) {
+          Shape s1 = new Shape();
+
+          s1.Area(4);
+
+
+     s1.Area(2, 5);
+ }
+}
