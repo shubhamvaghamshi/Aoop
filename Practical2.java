94295dc52e5359efe800fc6a08dbcfa17387b77c
shubhamvaghamshi <162270392+shubhamvaghamshi@users.noreply.github.com>
2025-04-04 11:58:13 +0530

    Create Practical2.java

---
+++ Practical2.java
@@ -0,0 +1,17 @@
+
+
+
+public class Practical2{
+  public static void main(String[] args) {
+    int p = 20;
+    int q = 10;
+    int r = 40;
+    if(p>q && p>r){
+       System.out.println("Max Number :- " + p);
+    }else if(q>p && q>r){
+         System.out.println("max Number:- " + q);
+     }else{
+         System.out.println("Max number:- " + r);
+    }
+  }   
+}
