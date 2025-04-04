6c4b0ae68ef46c62323ed589099ac7b5168ee1e2
shubhamvaghamshi <162270392+shubhamvaghamshi@users.noreply.github.com>
2025-04-04 12:12:58 +0530

    Create Practical9.java

---
+++ Practical9.java
@@ -0,0 +1,16 @@
+class Shubham {
+   
+    static int count = 0;
+
+  
+    static void incrementCount() {
+        count++;
+    }
+
+    public static void main(String[] args) {
+             incrementCount();
+      
+
+              System.out.println("Count: " + count);
+    }
+}
