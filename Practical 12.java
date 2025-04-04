9d0e3c854905b18cb7e963b79dc77f5291c15f6e
shubhamvaghamshi <162270392+shubhamvaghamshi@users.noreply.github.com>
2025-04-04 12:19:15 +0530

    Create Practical 12.java

---
+++ Practical 12.java
@@ -0,0 +1,27 @@
+class Rectangle {
+  int length;
+  int width;
+
+  
+  Rectangle() {
+      length = 5;
+      width = 10;
+      System.out.println("Default Constructor: Length = " + length + ", Width = " + width);
+  }
+
+  
+  Rectangle(int l, int w) {
+      length = l;
+      width = w;
+      System.out.println("Parameterized Constructor: Length = " + length + ", Width = " + width);
+  }
+}
+
+class SSS {
+  public static void main(String[] args) {
+      
+      Rectangle r1 = new Rectangle();
+
+            Rectangle r2 = new Rectangle(7, 12);
+  }
+}
