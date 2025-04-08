e733f26c4e7eb854822384b0c3477d29fb3b54bf
shubhamvaghamshi <162270392+shubhamvaghamshi@users.noreply.github.com>
2025-04-08 15:29:28 +0530

    Create Practical_17.java

---
+++ Practical_17.java
@@ -0,0 +1,25 @@
+
+class Shubham {
+   
+    void behaviour() {
+        System.out.println("na padya baad msti na krvi");
+    }
+}
+
+
+class Anger extends Shubham {
+ 
+    void moves() {
+        System.out.println("ignore kro guyssss");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+      Shubham myd = new Shubham();
+        myd.behaviour();  
+
+        Anger mined = new Anger();
+        mined.moves();  
+    }
+}
